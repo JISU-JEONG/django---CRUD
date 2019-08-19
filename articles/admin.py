a06b17590c7ed6e2155e7b856680045378bd0d7f
JISU_JEONG <jindex2411@naver.com>
2019-08-19 12:07:23 +0900

    02 | Article Model

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id','title','context','created_at','update_at')
+
+
+admin.site.register(Article, ArticleAdmin)
