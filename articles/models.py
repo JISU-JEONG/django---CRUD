10a1396bbf84ab06a6f34d161d3afae5c88bbd9a
JISU_JEONG <jindex2411@naver.com>
2019-08-20 10:03:13 +0900

    05 | articles - CRUD - R

--- articles/models.py
+++ articles/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # 데이터베이스 테이블을 정의하고,
 # 각각의 컬럼(필드) 정의
 class Article(models.Model):
-    # id : integer 자동으로 정의(Primary Key)
-    
+    # id : integer 자동으로 정의(Primary Key)  
+    # id = models.AutoField(primary_key=True) -> Integer 값이 자동으로 하나씩 증가(AUTOINCREMENT)
     # CharField - 필수인자로 max_length 지정
     title = models.CharField(max_length=10)
     context = models.TextField()
@@ -15,7 +15,7 @@ class Article(models.Model):
     #       auto_now : 수정시마다 자동으로 저장
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f'{self.id} : {self.title}'
 
